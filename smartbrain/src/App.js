04597e3fe8f324097635dd08fc1a7bac24cc6168
Charlie Tanner <charles.c.tanner@googlemail.com>
2020-08-27 21:14:14 +0100

    Deploy to Heroku.

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -101,7 +101,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input}, () => {
-      fetch('http://localhost:3000/image', {
+      fetch('https://ancient-tundra-36359.herokuapp.com/image', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -111,7 +111,7 @@ class App extends React.Component {
       .then(res => res.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://ancient-tundra-36359.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
